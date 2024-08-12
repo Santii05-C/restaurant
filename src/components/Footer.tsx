5dad21190af92594b3c7d9d3ebece5bf66521a0d
Santii05-C <santi.c.pgm@gmail.com>
2024-08-12 18:04:24 -0300

    last commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-12 md:h-24">
+      <Link href="/">MASSIMO</Link>
+      <p>© ALL RIGHTS RESERVED.</p>
+    </div>
+  );
 };
 
 export default Footer;
