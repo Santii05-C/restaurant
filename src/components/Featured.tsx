fceef427836ac7d203426927ddbda14a0a43a66e
Santii05-C <santi.c.pgm@gmail.com>
2024-08-13 17:37:06 -0300

    last commit

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -11,7 +11,7 @@ const Featured = () => {
         {featuredProducts.map((item) => (
           <div
             key={item.id}
-            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw]"
+            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
           >
             {/* IMAGE CONTAINER */}
             {item.img && (
@@ -20,9 +20,11 @@ const Featured = () => {
               </div>
             )}
             {/* TEXT CONTAINER */}
-            <div className="flex-1 flex flex-col items-center text-center gap-4">
-              <h1 className="text-xl font-bold uppercase">{item.title}</h1>
-              <p className="p-4">{item.desc}</p>
+            <div className="flex-1 flex flex-col items-center justify-center text-center gap-4 ">
+              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl ">
+                {item.title}
+              </h1>
+              <p className="p-4 2xl:p-8">{item.desc}</p>
               <span className="text-xl font-bold">${item.price}</span>
               <button className="bg-red-500 text-white p-2 rounded-md">
                 Add to Cart
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Offer = () => {
-  return <div>Offer</div>;
+  return (
+    <div className="">
+      {/* TEXT CONTAINER */}
+      <div className=""></div>
+      {/* IMAGE CONTAINER */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Offer;
