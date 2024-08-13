28ccc4fa38e9cd968320e88edb036ab775326b1b
Santii05-C <santi.c.pgm@gmail.com>
2024-08-13 17:53:30 -0300

    last commit

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,12 +1,31 @@
+import Image from "next/image";
 import React from "react";
 
 const Offer = () => {
   return (
-    <div className="">
+    <div className="bg-black h-screen flex flex-col md:flex-row">
       {/* TEXT CONTAINER */}
-      <div className=""></div>
+      <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
+        <h1 className="text-white text-5xl font-bold xl:text-6xl">
+          Delicious Burger & French Fry
+        </h1>
+        <p className="text-white xl:text-xl">
+          Progressively simplify effective e-toilers and process-centric methods
+          of empowerment. Quickly pontificate parallel.
+        </p>
+        <button className="bg-red-500 text-white rounded-md py-3 px-6">
+          Order Now
+        </button>
+      </div>
       {/* IMAGE CONTAINER */}
-      <div className=""></div>
+      <div className="flex-1 w-full relative">
+        <Image
+          src="/offerProduct.png"
+          alt=""
+          fill
+          className="object-contain "
+        />
+      </div>
     </div>
   );
 };
