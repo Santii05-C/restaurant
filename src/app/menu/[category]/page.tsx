f9cb1bfee6bdd3b49c49a211d2297cf581efa83f
Santii05-C <santi.c.pgm@gmail.com>
2024-08-14 18:32:22 -0300

    last commit

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -8,7 +8,7 @@ function CategoryPage() {
     <div className="flex flex-wrap text-red-500">
       {pizzas.map((item) => (
         <Link
-          className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4"
+          className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex  flex-col justify-between group even:bg-fuchsia-50"
           href={`/product/${item.id}`}
           key={item.id}
         >
@@ -19,15 +19,17 @@ function CategoryPage() {
             </div>
           )}
           {/* IMAGE CONYAINER */}
-          <div className="">
-            <h1 className="">{item.title}</h1>
-            <h2 className="">${item.price}</h2>
-            <button></button>
+          <div className="flex items-center justify-between font-bold ">
+            <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+            <h2 className="group-hover:hidden text-xl">${item.price}</h2>
+            <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">
+              Add to cart
+            </button>
           </div>
         </Link>
       ))}
     </div>
   );
 }
-
+//1:23
 export default CategoryPage;
