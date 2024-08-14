76daef77a9d16bac5e793a6f53041aa52b7298cd
Santii05-C <santi.c.pgm@gmail.com>
2024-08-14 11:09:17 -0300

    last commit

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import React from "react";
+import CountDown from "./CountDown";
 
 const Offer = () => {
   return (
-    <div className="bg-black h-screen flex flex-col md:flex-row">
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')]">
       {/* TEXT CONTAINER */}
       <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
         <h1 className="text-white text-5xl font-bold xl:text-6xl">
@@ -13,12 +14,13 @@ const Offer = () => {
           Progressively simplify effective e-toilers and process-centric methods
           of empowerment. Quickly pontificate parallel.
         </p>
+        <CountDown />
         <button className="bg-red-500 text-white rounded-md py-3 px-6">
           Order Now
         </button>
       </div>
       {/* IMAGE CONTAINER */}
-      <div className="flex-1 w-full relative">
+      <div className="flex-1 w-full relative md:h-full">
         <Image
           src="/offerProduct.png"
           alt=""
@@ -29,5 +31,5 @@ const Offer = () => {
     </div>
   );
 };
-//1:03
+
 export default Offer;
