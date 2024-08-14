91d732247a5130a5739597ae0ffe4f03e4a32a0d
Santii05-C <santi.c.pgm@gmail.com>
2024-08-14 11:38:06 -0300

    last commit

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,21 @@
+import { menu } from "@/data";
+import Link from "next/link";
 import React from "react";
 
 function MenuPage() {
-  return <div>MenuPage</div>;
+  return (
+    <div className=" p-4  lg:px-20 xl:px-40 h-[calc(100vh-9rem)] md:h-[calc(100vh-6rem)] flex flex-col md:flex-row items-center">
+      {menu.map((category) => (
+        <Link href={category.slug} key={category.id}>
+          <div className="">
+            <h1 className="">{category.title}</h1>
+            <p>{category.desc}</p>
+            <button className="">Explore</button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default MenuPage;
